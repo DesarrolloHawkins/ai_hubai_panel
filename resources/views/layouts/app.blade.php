48dda16b2e01d1b2f05c0165ff5f34a98e71631e
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2023-05-18 11:30:07 +0200

    New - Image nav user and fix name user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <img class="img-fluid rounded" src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png" alt="">{{ Auth::user()->nombre }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
