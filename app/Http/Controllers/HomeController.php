8bfd56e3a6633fe8e742c3450126e9af1023f9e4
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2023-05-19 09:50:10 +0200

    Fix - Home Controller OpenAI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use OpenAI\Laravel\Facades\OpenAI;
+// use OpenAI\OpenAI;
 
 class HomeController extends Controller
 {
@@ -26,8 +27,15 @@ class HomeController extends Controller
     {
 
         $result = OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'prompt' => 'PHP is',
+            'model' => 'davinci:ft-personal:apartamentos-hawkins-2023-04-27-09-45-29',
+            'prompt' => 'Donde estan las llaves ->',
+            'temperature' => 0.9,
+            "max_tokens"=> 200,
+            "top_p"=> 1,
+            "frequency_penalty"=> 0,
+            "presence_penalty"=> 0,
+            "stop"=> ["_END"]
+
         ]);
         
         echo $result['choices'][0]['text']; // an open-source, widely-used, server-side scripting language.
