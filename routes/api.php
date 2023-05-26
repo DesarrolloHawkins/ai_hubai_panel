366e1ca2faff690ca1b27b6559453518e3fbaeac
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2023-05-26 11:52:17 +0200

    Revert "Api"
    This reverts commit 22ceeab3f16495f3c11b78e99b8036c8e431bb81.

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/twilio/voice-response', [App\Http\Controllers\TwilioController::class, 'voiceResponse'])->name('twilio.voice-response');
